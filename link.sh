4124e6b0a95a9e633aa724564a4dd0bf19b43378
agatan <m.k.mahoo@gmail.com>
2016-03-24 22:33:15 +0900

    Vim setting refactor

--- link.sh
+++ link.sh
@@ -2,15 +2,12 @@
 
 echo 'making symbolic links...'
 
-for dot in .??*
+ln -s ${HOME}/dotfiles/vim ${HOME}/.vim
+
+for dot in '.zshrc'
 do
-    if [ ${dot} = ".git" -o ${dot} = ".DS_Store" ]; then
-        continue
-    fi
     ln -s ${HOME}/dotfiles/${dot} ${HOME}/${dot}
 done
 
 mkdir -p ~/.config/nvim
 ln -s ${HOME}/dotfiles/init.vim ${HOME}/.config/nvim/init.vim
-
-ln -s ${HOME}/dotfiles/vim ${HOME}/.vim
