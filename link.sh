0f7a147c48f6abc0aca74f66c4e37e1ea51e5d39
agatan <agatan039@gmail.com>
2016-09-19 10:40:46 +0900

    Update git config

--- link.sh
+++ link.sh
@@ -4,7 +4,7 @@ echo 'making symbolic links...'
 
 ln -s ${HOME}/dotfiles/vim ${HOME}/.vim
 
-for dot in '.zshrc'
+for dot in '.zshrc' '.gitignore' '.gitconfig'
 do
     ln -s ${HOME}/dotfiles/${dot} ${HOME}/${dot}
 done
