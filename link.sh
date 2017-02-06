126306324a4bba2ae1dd28aeeabfbe266cdf117c
agatan <agatan039@gmail.com>
2017-02-06 23:50:37 +0900

    Show battery info in tmux status bar

--- link.sh
+++ link.sh
@@ -4,7 +4,7 @@ echo 'making symbolic links...'
 
 ln -s ${HOME}/dotfiles/vim ${HOME}/.vim
 
-for dot in '.zshrc' '.gitignore' '.gitconfig' '.editorconfig' '.tmux.conf'
+for dot in '.zshrc' '.gitignore' '.gitconfig' '.editorconfig' '.tmux.conf' '.tmux'
 do
     ln -s ${HOME}/dotfiles/${dot} ${HOME}/${dot}
 done
