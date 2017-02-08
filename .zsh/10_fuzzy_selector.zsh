962dfdbe849be13c829012409e94bcde473c23e3
agatan <agatan039@gmail.com>
2017-02-08 22:11:36 +0900

    Add keybinds.zsh

--- .zsh/10_fuzzy_selector.zsh
+++ .zsh/10_fuzzy_selector.zsh
@@ -11,22 +11,20 @@ if is_tmux_running; then
     alias fzf=fzf-tmux
 fi
 
-_fzf-ghq-cd() {
+fzf-ghq-cd() {
     local selected_dir=$(ghq list --full-path | fzf --query "$LBUFFER")
     if [ -n "$selected_dir" ]; then
         BUFFER="cd $selected_dir"
         zle accept-line
     fi
 }
-zle -N _fzf-ghq-cd
-bindkey '^x^g' _fzf-ghq-cd
+zle -N fzf-ghq-cd
 
-_fzf-history() {
+fzf-history() {
     BUFFER=`history -n 1 | awk '!a[$0]++' | fzf --tac --no-sort --query "$LBUFFER"`
     CURSOR=$#BUFFER
 }
-zle -N _fzf-history
-bindkey '^r' _fzf-history
+zle -N fzf-history
 
 killp() {
     ps | tail -n +2 | \
@@ -37,4 +35,3 @@ killp() {
         kill -KILL $pid
     done
 }
-
---
+++ .zsh/20_keybinds.zsh
@@ -0,0 +1,6 @@
+bindkey -d
+bindkey -v
+
+bindkey '^x^g' fzf-ghq-cd
+
+bindkey '^r' fzf-history
